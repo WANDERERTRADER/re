09b210a1bc050b5465849a0c592138596ea4723a
Jane Doe <janedoe@example.com>
2025-07-17 14:08:13 +0700

    inital project version

--- demo-http-server.js
+++ demo-http-server.js
@@ -4,7 +4,13 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
+    if (req.url === '/about') {
+            res.end('About Page\n');
+    } else if(req.url === '/contact') {
+            res.end('Contact Page\n');
+    } else {
     res.end('Hello, World!\n');
+    }
 });
 
 server.listen(port, hostname, () => {
